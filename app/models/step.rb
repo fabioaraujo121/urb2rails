b125836b40da2a843546a03d22296c1ac4d74e6c
Fábio Araújo <fabioaraujo121@gmail.com>
2019-09-12 11:25:36 -0300

    Atualizando status do processo de acordo com o passo dado

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,11 @@ class Step < ApplicationRecord
   	belongs_to :kind_step
   	belongs_to :demand
   	belongs_to :user
+
+  	after_save do
+  		# Set process to processing
+  		self.demand.processing! if self.kind_step.valid_time and (self.demand.opened? or self.demand.stopped?)
+  		# Stop the proccess if it's not a valid time
+  		self.demand.stopped! unless self.kind_step.valid_time
+  	end
 end
